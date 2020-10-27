5260426e78287076ae695310ff58c22c163d6e35
Boingzns <zebsummerfield@gmail.com>
2020-10-27 17:18:36 +0000

    fffgk

--- Pong.py
+++ Pong.py
@@ -13,6 +13,7 @@ class Paddle:
         self.y = y
         self.movex = 0
         self.score = 0
+        self.speed = 2
 
     def draw(self, canvas):
         if self.x <= 0 or self.x >= 360:
@@ -22,17 +23,18 @@ class Paddle:
 
 class Ball:
     def __init__(self, x, y):
+        self.colour = colour()
         self.x = x
         self.y = y
-        self.movex = 0.20
-        self.movey = 0.15
+        self.movex = 2.5
+        self.movey = 1.5
         self.scored = False
         self.wait = False
 
     def draw(self, canvas):
         self.x += self.movex
         self.y += self.movey
-        canvas.create_oval(self.x, self.y, self.x+20, self.y+20, fill = colour())
+        canvas.create_oval(self.x, self.y, self.x+20, self.y+20, fill = self.colour)
 
     def collisions(self, paddle1, paddle2):
         if self.x <= 0 or self.x >= 380:
@@ -41,16 +43,18 @@ class Ball:
             self.movey *= -1
             paddle2.score += 1
             self.scored = True
+            self.colour = colour()
         if self.y >= 380:
             self.movey *= -1
             paddle1.score += 1
             self.scored = True
+            self.colour = colour()
         if self.y <= 20 and self.x+10 >= paddle1.x and self.x+10 <= paddle1.x+40:
-            self.movey = 0.1
+            self.movey *= -1
             #if round(self.y,2) == 20:
             #    self.movex *= (random.randint(5,15)/10)
         if self.y >= 360 and self.x+10 >= paddle2.x and self.x+10 <= paddle2.x+40:
-            self.movey = -0.1
+            self.movey *= -1
             #if round(self.y,2) == 360:
             #    self.movex *= (random.randint(5,15)/10)
         
@@ -68,6 +72,7 @@ class GameWindow(Frame):
         self.paddle1 = Paddle(180,10)
         self.paddle2 = Paddle(180,380)
         self.ball = Ball(190,190)
+        self.speed = 2
 
         #Frame(self,height=10).pack()
         self.score_box = Label(self, height = 1, width = 10,
@@ -101,22 +106,31 @@ class GameWindow(Frame):
         
 
     def A(self, event):
-        self.paddle1.movex =-0.15
+        self.paddle1.movex =-self.paddle1.speed
     def D(self, event):
-        self.paddle1.movex = 0.15
+        self.paddle1.movex = self.paddle1.speed
     def Left_Arrow(self, event):
-        self.paddle2.movex = -0.15
+        self.paddle2.movex = -self.paddle2.speed
     def Right_Arrow(self, event):
-        self.paddle2.movex = 0.15
+        self.paddle2.movex = self.paddle2.speed
 
 
 def main():
     master = Tk()
     master.geometry("500x500+100+100")
     game = GameWindow(master)
+    
     while True:
+        start = time.time()
         master.update()
         game.draw()
+
+        #introduce wait till delay between redraws
+        end = time.time()
+        length = end - start
+        while length < 0.01:
+            end = time.time()
+            length = end - start
 
 if __name__ == '__main__':
     main()
